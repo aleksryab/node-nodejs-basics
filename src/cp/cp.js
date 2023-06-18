a76bf49b4ba5b9c664a739f1665966e82a582d99
aleksryab <aleksryabtsev@gmail.comm>
2023-06-18 19:29:42 +0700

    feat: implement spawnChildProcess function

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,15 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { fork } from 'node:child_process';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+  const scriptPath = join(__dirname, 'files', 'script.js');
+  const childProcess = fork(scriptPath, args);
+  childProcess.on('exit', () => console.log('Process was closed'));
+  childProcess.on('error', (err) => console.error(err.message));
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['hi', 'reviewer', 'good', 'luck']);
