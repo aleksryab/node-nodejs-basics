e43ac718ce96e53f43ab2cbe17c5c2f7af5552a6
aleksryab <aleksryabtsev@gmail.comm>
2023-06-18 12:17:03 +0700

    feat: implement parseEnv function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
 const parseEnv = () => {
-    // Write your code here 
+  const prefix = /^RSS_/;
+
+  Object.entries(process.env).forEach(([key, value]) => {
+    if (prefix.test(key)) {
+      console.log(`${key}=${value};`);
+    }
+  });
 };
 
 parseEnv();
