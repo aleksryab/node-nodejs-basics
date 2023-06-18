3efb3ba85a42b13d5c5f0d15a77828f724435a6b
aleksryab <aleksryabtsev@gmail.comm>
2023-06-18 14:26:52 +0700

    feat: implement read function

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,15 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { createReadStream } from 'node:fs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fileToRead.txt');
+  const stream = createReadStream(filePath, { encoding: 'utf-8' });
+
+  stream.on('data', (chunk) => process.stdout.write(chunk));
+  stream.on('error', (error) => console.error(error.message));
 };
 
 await read();
