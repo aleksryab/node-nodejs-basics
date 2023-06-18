e1de2fd8b1dc47cbfdd5d010f33df8da4c5c2786
aleksryab <aleksryabtsev@gmail.comm>
2023-06-18 14:13:54 +0700

    feat: implement calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,15 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { readFile } from 'fs/promises';
+import { createHash } from 'crypto';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+  const data = await readFile(filePath);
+  const hash = createHash('sha256').update(data).digest('hex');
+  console.log(hash);
 };
 
 await calculateHash();
