9e4131b2e25e52a7edbcde9882aec6b6e82b620e
aleksryab <aleksryabtsev@gmail.comm>
2023-06-18 15:00:42 +0700

    feat: implement write function

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import { createWriteStream } from 'node:fs';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, 'files', 'fileToWrite.txt');
+  const outputStream = createWriteStream(filePath, { encoding: 'utf-8' });
+
+  process.stdout.write('Hi! Write your message:\n');
+  process.stdin.on('data', (data) => {
+    outputStream.write(data.toString());
+  });
+
+  outputStream.on('error', (error) => console.error(error.message));
 };
 
 await write();
