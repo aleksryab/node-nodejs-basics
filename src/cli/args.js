f8d1f1f1637987227fe1fd30ba6bf2fcda819be5
aleksryab <aleksryabtsev@gmail.comm>
2023-06-18 13:33:54 +0700

    feat: implement parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
 const parseArgs = () => {
-    // Write your code here 
+  const argPrefix = /^--/;
+  const args = process.argv.slice(2);
+
+  while (args.length) {
+    const currentArg = args.shift();
+    let currentValue;
+
+    if (argPrefix.test(currentArg)) {
+      if (!argPrefix.test(args[0])) {
+        currentValue = args.shift();
+      }
+      console.log(`${currentArg.replace(argPrefix, '')} is ${currentValue};`);
+    }
+  }
 };
 
 parseArgs();
